4302c3be09c56105c08a05998c7b8fbeb53671fe
mathncode-sid <sidneybarakamuriuki1@gmail.com>
2025-08-04 00:40:40 +0300

    Added comments for guidance

--- bot.py
+++ bot.py
@@ -16,12 +16,14 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
+# Message handler to handle astrological signs when the user inputs a zodiac sign
 @bot.message_handler(commands=["horoscope"])
 def sign_handler(message):
     text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
     sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
     bot.register_next_step_handler(sent_msg, day_handler)
 
+# Message handler to take care of the date
 def day_handler(message):
     sign = message.text
     text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
@@ -30,6 +32,7 @@ def day_handler(message):
     bot.register_next_step_handler(
         sent_msg, fetch_horoscope, sign.capitalize())
 
+# Message handler that fetches horoscope data from the horoscope API
 def fetch_horoscope(message, sign):
     day = message.text
     horoscope = get_daily_horoscope(sign, day)
